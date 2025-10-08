321a4e6e5769cd757656b594b0c532bbfe1b2070
Prannjjol Pratyussh <prannjjol33@gmail.com>
2025-10-09 01:30:31 +0530

    Added mini-kafka project

---
+++ mini-kafka/src/main/java/com/streamlite/mini_kafka/MiniKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.streamlite.mini_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniKafkaApplication.class, args);
+	}
+
+}
